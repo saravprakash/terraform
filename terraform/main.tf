a4aaa900ded02f4c0cc0ade55ba86a0b32fbc71a
saravprakash@gmail.com <saravprakash@gmail.com>
2024-07-17 19:37:43 +0530

    setup remote state in storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,15 @@ provider "azurerm" {
     skip_provider_registration = true
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-infra"
+    storage_account_name = "rmtfstatefiles"                
+    container_name       = "tfstatefiles"            
+    key                  = "app01rmstatefile.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "rg-01" {
   name     = "dev-si-app-01"
   location = "South India"

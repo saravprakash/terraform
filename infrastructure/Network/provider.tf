e834d57492fc19684a953c366d1d879b582fab7e
BS Prakash <saravprakash@gmail.com>
2026-07-12 18:03:15 +0530

    update terraform plan and apply workflow

--- infrastructure/Network/provider.tf
+++ infrastructure/Network/provider.tf
@@ -1,16 +1,18 @@
-provider "azurerm" {
-    features {
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
     }
 
-    skip_provider_registration = true
+  }
+  backend "azurerm" {}
 }
 
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "terraform-infra"
-#     storage_account_name = "tfmtfstatefiles"                
-#     container_name       = "tfstatefiles"            
-#     key                  = "app01rmstatefile.tfstate"
-#   }
-# }
+provider "azurerm" {
+  features {
+  }
+
+  skip_provider_registration = true
+}
+
 

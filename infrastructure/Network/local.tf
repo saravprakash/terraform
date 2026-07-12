5e9177f01955e3ecb0deffd91c9269d9ab2f9040
BS Prakash <saravprakash@gmail.com>
2026-07-12 17:29:53 +0530

    update terraform plan and apply workflow

---
+++ infrastructure/Network/local.tf
@@ -0,0 +1,3 @@
+locals{
+    resource_group_name =  lower("${var.environment}-si-app-01")
+}
--- infrastructure/Network/rg.tf
+++ infrastructure/Network/rg.tf
@@ -1,14 +1,14 @@
 resource "azurerm_resource_group" "rg-01" {
-  name     = "dev-si-app-01"
+  name     = local.resource_group_name
   location = "South India"
 }
 
-resource "azurerm_resource_group" "rg-02" {
-  name     = "int-si-app-01"
-  location = "South India"
-}
+# resource "azurerm_resource_group" "rg-02" {
+#   name     = "int-si-app-01"
+#   location = "South India"
+# }
 
-resource "azurerm_resource_group" "rg-03" {
-  name     = "uat-si-app-01"
-  location = "South India"
-}
+# resource "azurerm_resource_group" "rg-03" {
+#   name     = "uat-si-app-01"
+#   location = "South India"
+# }
